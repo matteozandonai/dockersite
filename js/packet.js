bafdebd001954b9bb45f477d5e1f43f06694de66
Matteo Zandonai <matteo.zandonai@marconi.loc>
2021-05-07 15:59:53 +0200

    modifiche nais nais

--- js/packet.js
+++ js/packet.js
@@ -1,3 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
